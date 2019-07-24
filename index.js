d9897d50725d65e5cbfad7b267d0af1360045d66
Gaston <gaston.silva@arbusta.com>
2019-07-24 16:46:56 -0300

    Aceptar un parametro para iterar

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-//Esto es un comentario 
-const name = process.argv.lenght > 2 ? process.argv[2] : 'Arbusta'
-console.log('Hola ${name}')
+// El nombre default es 'Arbusta'
+const name = process.argv.length > 2 ? process.argv[2] : 'Arbusta'
+const times = process.argv.length > 3 ? process.argv[3] : 10
+
+for (let i = 0; i < times; i++) {
+ console.log(`Hola ${name} por vez número ${i + 1}`)
+}
