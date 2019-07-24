2efe330ea02c5e4bc4128b24b2dff52bda707f42
Gaston <gaston.silva@arbusta.com>
2019-07-24 16:57:30 -0300

    Se agrego logger.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var Logger = require('./logger.js')
+
 // El nombre default es 'Arbusta'
 const name = process.argv.length > 2 ? process.argv[2] : 'Arbusta'
 const times = process.argv.length > 3 ? process.argv[3] : 10
 
+const logger = new Logger()
+
 for (let i = 0; i < times; i++) {
- console.log(`Hola ${name} por vez número ${i + 1}`)
+ logger.print(`Hola ${name} por vez número ${i + 1}`)
 }
